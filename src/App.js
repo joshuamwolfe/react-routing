5db0df429761f936b822ef4824ed827802a38519
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-02-04 09:31:43 -0500

    updated app for better routing method

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Home from './Home';
-import Eat from './Eat';
-import Drink from './Drink';
-
 function App() {
-  const [page, setPage] = useState ('home');
-
-  const showPage = () => {
-    if(page === 'home') return <Home/>
-    if(page === 'eat') return <Eat/>
-    if(page === 'drink') return <Drink/>
-  }
-
   return (
     <main className='App'>
-      <nav>
-        <a onclick={() => setPage('home')}>Home</a>
-        <a onclick={() => setPage('eat')}>Eat</a>
-        <a onclick={() => setPage('drink')}>Drink</a>
-      </nav>
-      {showPage()}
+      <BrowserRouter>
+        
+      </BrowserRouter>
     </main>
   );
 }
